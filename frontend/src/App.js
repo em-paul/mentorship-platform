bf06a4f6f169fe0991eadec57388f3673a8592db
Emily Paul <52010064+em-paul@users.noreply.github.com>
2021-04-17 12:03:20 -0400

    Add top bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
 import './mentee-homepage-tabs.css';
 import { MenteeHomepage } from './MenteeHomepage';
 import './App.css';
+import { IoPersonCircleSharp } from 'react-icons/io5';
+import Logo from './PA_logo_color_dark.png';
+
+const TopBar = () => {
+  return (
+    <div className = 'top-bar'>
+      <img  src = {Logo} alt = "Project Access logo"
+      style = {{height: '7vh', position: 'absolute', top: '0', left: '0', marginLeft: '3vh'}} />
+      <a href = "https://projectaccess.org/home-page2021a" 
+      style = {{margin: '1em', color: 'black'}}> Project Access Website </a>
+      <a> | </a>
+      <a href = "https://projectaccess.org/home-page2021a" 
+      style = {{margin: '1em', color: 'black'}}> Help </a>
+      <a> | </a>
+      <a href = "https://projectaccess.org/home-page2021a" 
+      style = {{margin: '1em', color: 'black'}}> Profile Settings </a>
+      <IoPersonCircleSharp size = '4em' />
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div>
+    <div style = {{display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-end'}} >
+      <TopBar />
       <MenteeHomepage />
     </div>
     
