54b4179dd1b0661b71a84f2ffbd42fed938dc185
Emily Paul <52010064+em-paul@users.noreply.github.com>
2021-05-22 11:28:25 -0400

    Delete extraneous files

--- frontend/src/components/dashboard/Dashboard.js
+++
@@ -1,4 +0,0 @@
-import { useState, useEffect, useCallback, CSSProperties } from 'react';
-import { useVersion, useDataProvider } from 'react-admin';
-import { useMediaQuery, Theme } from '@material-ui/core';
-
