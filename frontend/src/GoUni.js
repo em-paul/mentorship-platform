089032ab6838b9742108c2da041f5f6afff4ded2
Emily Paul <52010064+em-paul@users.noreply.github.com>
2021-06-03 18:25:02 -0400

    Fix stand-in link for 'Going to University' in Access Academy tab

--- frontend/src/GoUni.js
+++ frontend/src/GoUni.js
@@ -7,8 +7,9 @@ export const GoCard = () => {
     return (
         <div className = 'modules-card'>
             <h4 style = {{fontWeight: 'bold'}} > Going to University </h4>
-            <div className = 'contents' >
-                Look through <a href = 'https://projectaccess.org/going-to-university'> this page </a>
+            <div style = {{display: 'inline'}} >
+                Look through 
+                <a href = 'https://projectaccess.org/going-to-university'> this page </a> 
                 for insights on life at various different universities in the UK and US!
             </div>
         </div>
